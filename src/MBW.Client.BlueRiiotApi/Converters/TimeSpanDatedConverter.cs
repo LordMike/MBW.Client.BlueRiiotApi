c8bf2ffe4918fe7804c5205d1d37b7d093af24f2
Michael Bisbjerg <michael@mbwarez.dk>
2021-04-16 13:20:03 +0200

    Ensure we can parse invalid schedule ranges
    See https://github.com/LordMike/MBW.BlueRiiot2MQTT/issues/44#issuecomment-821017234

--- src/MBW.Client.BlueRiiotApi/Converters/TimeSpanDatedConverter.cs
+++ src/MBW.Client.BlueRiiotApi/Converters/TimeSpanDatedConverter.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 
 namespace MBW.Client.BlueRiiotApi.Converters
 {
     internal class TimeSpanDatedConverter : JsonConverter<TimeSpan>
     {
+        // Example: 0000-01-01T08:00:00.000, unparseable by DateTime due to year `0000`
+        private readonly Regex _parsingRegex = new(@"^[0-9\-]+T([0-9\:\.]+)$", RegexOptions.Compiled);
+
         public override void WriteJson(JsonWriter writer, TimeSpan value, JsonSerializer serializer)
         {
             // BlueRiiot stores these timespans as datetimes. So any date is just as valid as the next.
@@ -14,13 +18,22 @@ namespace MBW.Client.BlueRiiotApi.Converters
 
         public override TimeSpan ReadJson(JsonReader reader, Type objectType, TimeSpan existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
-            if (!(reader.Value is DateTime asDateTime))
+            switch (reader.Value)
             {
-                // https://github.com/LordMike/MBW.BlueRiiot2MQTT/issues/44
-                throw new Exception($"Unable to parse value as TimeSpan. Received: '{reader.Value}'");
+                case string asString:
+                {
+                    Match matched = _parsingRegex.Match(asString);
+                    string timespanValue = matched.Groups[1].Value;
+                    TimeSpan timespan = TimeSpan.Parse(timespanValue);
+
+                    return timespan;
+                }
+                case DateTime asDateTime:
+                    return asDateTime.TimeOfDay;
+                default:
+                    // https://github.com/LordMike/MBW.BlueRiiot2MQTT/issues/44
+                    throw new Exception($"Unable to parse value as TimeSpan. Received: '{reader.Value}'");
             }
-            
-            return asDateTime.TimeOfDay;
         }
     }
 }
